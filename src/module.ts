647026d356c4c45f2d771091f8eaae8084536daf
Caio Emidio <caio.emidio@gmail.com>
2024-05-01 16:15:13 +0100

    first commit

---
+++ src/module.ts
@@ -0,0 +1,40 @@
+import { defineNuxtModule, addPlugin, createResolver, addComponentsDir, installModule } from '@nuxt/kit'
+
+// Module options TypeScript interface definition
+export interface ModuleOptions { }
+
+export default defineNuxtModule<ModuleOptions>({
+  meta: {
+    name: 'monkeyDevUI',
+    configKey: 'MkyDevUI',
+  },
+  // Default configuration options of the Nuxt module
+  defaults: {},
+  async setup(options, nuxt) {
+
+    const { resolve } = createResolver(import.meta.url)
+    // We can inject our CSS file which includes Tailwind's directives
+    nuxt.options.css.push(resolve('./runtime/assets/css/tailwind.css'))
+
+    await installModule('@nuxtjs/tailwindcss', {
+      // module configuration
+      exposeConfig: true,
+      config: {
+        darkMode: 'class',
+        content: {
+          files: [
+            resolve('./runtime/components/**/*.{vue,mjs,ts}'),
+            resolve('./runtime/*.{mjs,js,ts}')
+          ]
+        }
+      }
+    })
+
+    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
+    addPlugin(resolve('./runtime/plugin'))
+
+    addComponentsDir({
+      path: resolve('runtime/components')
+    })
+  },
+})
---
+++ src/runtime/components/Mky/btn.vue
@@ -0,0 +1,5 @@
+<template>
+    <button class="bg-red-500 py-y px-4">
+        OI CAIO IMPERIAL
+    </button>
+</template>
